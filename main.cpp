c3caca3dcb6f695dae50f180255a1fd6c3498770
Eric Phung <esphung@iupui.edu>
2015-03-01 22:09:57 -0500

    Beat version is Finished

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ using namespace std;
 
 
 
+
+
 string argument = "Hello World";
 
 //======================================
@@ -29,49 +31,30 @@ std::istream& operator >> (std::istream& str, TextInfo& data){
 	return str;
 } // end input stream
 
-
-
-
 //======================================
-
+// METHODS
+void showStudentsBy(std::vector<Student> heapArray);
+void menu();
+void showDatabase(std::vector<Student> heapArray);
 
 
 int main(){
-	//string databaseFile			=	"data.dat";
-	std::ifstream							file("data.dat");
+	int count = 1;
+	string dbFile = "data.dat";
+	std::ifstream							file(dbFile);
 	TextInfo::TextInfo									row;
-	TextInfo::TextInfo v;
-
-	Student *newStudent = new Student();// create studen array
-
-	int i;
+	std::vector<Student> heapArray; // vectors, by default, allocate elements from the heap
+	menu(); // for user to choose menu option
 	while(file >> row){
-		// orginal code
-		newStudent = new Student(row); // creat new student
-		++i; // increment student count
-
-
-
-
-
-
-
-
-
-
-
-
-
-		cout << "Number of passes: " << i << endl;
-		cin.ignore();
-
-
-
+		Student newStudent = Student(row);
+		heapArray.push_back(newStudent);
+		++count; // increment student count
 
 	}// end while fill stream --> row
-	//std::cout << typeid(newStudent).name() << '\n';// DEBUG
 
-	delete newStudent; // delete student array
+
+	//showStudentsBy(heapArray);
+	showDatabase(heapArray);
 
 
 
@@ -82,8 +65,45 @@ int main(){
 	return 0;
 }// end main
 
+void menu(){
+	cout << "\t\tHello Database" << endl;
+	cout << "(d)isplay database, (s)ort database, (a)dd new student, (q)uit" << endl;
+	cin.ignore();
+}// end menu function
+
+
+void showDatabase(std::vector<Student> heapArray){
+		if (false){
+			//cout << "Number of passes: " << count << endl;
+			for (int i = 0; i < heapArray.size(); ++i){
+				heapArray[i].displayData();
+				cout << heapArray[i].birthdayObject.date << endl;
+				cout << heapArray[i].completionObject.date << endl;
+			} // end for
+		} // end if for user display all students
+
+}
+
+
+void showStudentsBy(std::vector<Student> heapArray){
+	cout << "========   SORTED!!! ========" << endl;
+	enum Sort {ALL, NAME, CITY, GRADE };
+	//Sort show = NAME;
+	Sort show = ALL;
+
+	switch(show) {
+		case NAME  : 	for (int i = 0; i < heapArray.size(); ++i){
+			cout << heapArray[i].first_name << endl;
+		}; break; // name case
+		case CITY : for (int i = 0; i < heapArray.size(); ++i){
+			cout << heapArray[i].addressObject.city + ", " + heapArray[i].addressObject.state << endl;
+		}; break; // address case
+		case GRADE  : for (int i = 0; i < heapArray.size(); ++i){
+			cout << heapArray[i].last_name << ", " << heapArray[i].first_name + "\t(" + heapArray[i].gpa << ")" << endl;
+	}; break; // grade case
+		default : cout << "Oopsie!" << endl; break;
+}
 
 
 
-
-
+} // end show names
--- student.cpp
+++ student.cpp
@@ -8,68 +8,40 @@
 
 
 Student::Student(){
-	std::cout << "null" << std::endl;
+	//std::cout << "null" << std::endl;
 }// end null student constructor
 
 Student::Student(TextInfo& row){
-	std::cout << "OVERloaded student" << std::endl;
-
+	//std::cout << "OVERloaded student" << std::endl;
 	Student::row = row;
-
 	// Student object contains one address object
-	Student::addressObject = Address(row[2],row[3],row[4],row[5],row[6]);
+
+
+	last_name = row[0];
+	first_name = row[1];
+	first_address = row[2];
+	second_address = row[3];
+	city = row[4];
+	state = row[5];
+	zipcode = row[6];
+	birthday_date = row[7];
+	completion_date = row[8];
+	gpa = row[9];
+	credit_hours = row[10];
+
 	// student has 2 date objects
-
-	Student::birthdayObject = Date(row[7]);
-	Student::completionObject = Date(row[8]);
-	Student::last_name = row[0];
-	Student::first_name = row[1];
-	Student::first_address = row[2];
-	Student::second_address = row[3];
-	Student::city = row[4];
-	Student::state = row[5];
-	Student::zipcode = row[6];
-	Student::birthday_date = row[7];
-	Student::completion_date = row[8];
-	Student::gpa = row[9];
-	Student::credit_hours = row[10];
-
-
-	std::cout << "  ========================" << std::endl;
-	std::cout << "\t" << row[1] << " " << row[0] << " was found." << std::endl;
+	Student::birthdayObject = Date(birthday_date);
+	Student::completionObject = Date(completion_date);
+	// student has one address object
+	Student::addressObject = Address(first_address,second_address,city,state,zipcode);
 	displayData();
 }// end overload constructor
-
-
-
-/*
-Student::Student(std::vector<std::string> myKeysVector){
-	std::cout << "OVERloaded student" << std::endl;
-	// Student object contains one address object
-	Student::addressObject = Address(myKeysVector.at(2),myKeysVector.at(3),myKeysVector.at(4),myKeysVector.at(5),myKeysVector.at(6));
-	// student has 2 date objects
-	Student::birthdayObject = Date(myKeysVector.at(7));
-	Student::completionObject = Date(myKeysVector.at(8));
-	Student::last_name = myKeysVector.at(0);
-	Student::first_name = myKeysVector.at(1);
-	Student::first_address = myKeysVector.at(2);
-	Student::second_address = myKeysVector.at(3);
-	Student::city = myKeysVector.at(4);
-	Student::state = myKeysVector.at(5);
-	Student::zipcode = myKeysVector.at(6);
-	Student::birthday_date = myKeysVector.at(7);
-	Student::completion_date = myKeysVector.at(8);
-	Student::gpa = myKeysVector.at(9);
-	//Student::credit_hours = myKeysVector.at(9);
-	std::cout << "  ========================" << std::endl;
-	std::cout << "\t" << myKeysVector.at(1) << " " << myKeysVector.at(0) << " was found." << std::endl;
-	displayData();
-}// end overload constructor
-
-*/
 
 Student::~Student(){}// end student destructor
+
 void Student::displayData(){
+	std::cout << "  ========================" << std::endl;
+	std::cout << "\t" << first_name << " " << last_name << " was found." << std::endl;
 	std::cout << "Last:\t\t" << last_name << std::endl;
 	std::cout << "First:\t\t" << first_name << std::endl;
 	std::cout << "Address:\t" << addressObject.first_address << std::endl;
